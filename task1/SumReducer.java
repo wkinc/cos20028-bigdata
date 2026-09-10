b78944e411efaa6d08f4294c830a8bc554fe9bab
kinc <125807080+wkinc@users.noreply.github.com>
2026-09-10 14:05:55 +0800

    Create SumReducer.java

---
+++ task1/SumReducer.java
@@ -0,0 +1,28 @@
+package task1;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+	private static final int shift = -20;
+
+  @Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException {
+		int wordCount = 0;
+		
+		for (IntWritable value : values) {
+		  
+			wordCount += value.get();
+		}
+		
+		int shiftedCount = wordCount + shift;
+					
+		context.write(key, new IntWritable(shiftedCount));
+	}
+}
+
