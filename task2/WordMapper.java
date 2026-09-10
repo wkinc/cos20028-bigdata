ddf1e3d151e12f15f5672e7fd57bafa507f3386d
kinc <125807080+wkinc@users.noreply.github.com>
2026-09-10 13:13:45 +0800

    Implement WordMapper class for Hadoop MapReduce

---
+++ task2/WordMapper.java
@@ -0,0 +1,24 @@
+package stubs;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+  private final static IntWritable one = new IntWritable(1);
+	private final static Text linekey = new Text("Lines");
+
+  @Override
+  public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
+
+    String line = value.toString();
+
+   
+    context.write(linekey, one);
+  }
+}
