b50c8665b7a9fe661e7ffce03335aac56c9adf55
kinc <125807080+wkinc@users.noreply.github.com>
2026-09-19 21:39:37 +0800

    sumreducer

---
+++ assignment_1/part_2/SumReducer.java
@@ -0,0 +1,24 @@
+package assignment1_2;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+  @Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException {
+		int numberCount = 0;
+		
+		for (IntWritable value : values) {
+		  
+			numberCount += value.get();
+		}
+		
+		context.write(key, new IntWritable(numberCount));
+	}
+}
+
